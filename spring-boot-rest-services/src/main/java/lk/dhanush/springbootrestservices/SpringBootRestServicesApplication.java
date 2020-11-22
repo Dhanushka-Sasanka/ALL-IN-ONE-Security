50c10674575ab1e10b3711c1b1faba29fae06a1d
Dhanushka_Sasanka <dhanushkasasankapayza@gmal.com>
2020-11-22 14:22:15 +0530

    all committed.

---
+++ spring-boot-rest-services/src/main/java/lk/dhanush/springbootrestservices/SpringBootRestServicesApplication.java
@@ -0,0 +1,13 @@
+package lk.dhanush.springbootrestservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestServicesApplication.class, args);
+    }
+
+}
